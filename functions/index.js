3d5706409439f149aa918056ec3d0287ed8e2dff
Jonathan Campos <jonbcampos@gmail.com>
2018-07-29 19:32:47 -0500

    working on functions

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+export function eventSearchHandler(agent) {
+    agent.add(`not complete`);
+}
+
+export function eventSearchImplicitHandler(agent) {
+    agent.add(`not complete`);
+}
+
+export function eventTicketsBookHandler(agent) {
+    agent.add(`not complete`);
+}
